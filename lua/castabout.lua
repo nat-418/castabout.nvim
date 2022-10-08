8e13365c818edea133e148714ed5c4edeb6460ed
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-10-08 15:30:20 +0200

    Update README

--- lua/castabout.lua
+++ lua/castabout.lua
@@ -18,16 +18,24 @@ M.search = function(direction)
     return 0
   end
 
-  return vim.ui.input({prompt = message, default = '' }, callback)
+  return vim.ui.input({prompt = message, default = ''}, callback)
 end
 
+-- A common convention for Neovim plugins written in Lua is to wrap the
+-- loading of commands and any user configuration into a `setup` function.
+-- This way, the end-user has more control over when and where they are loaded.
 M.setup = function()
+  -- Custom command completion requires a callback that takes three arguments.
+  -- In this case, we don't care what they are and just want to return a list
+  -- of subcommands.
   local completion = function(_, _, _) return {'forwards', 'backwards'} end
 
   return vim.api.nvim_create_user_command(
-    'Castabout',
-    function(args) M.search(args) end,
-    {nargs = '?', complete = completion}
+    'Castabout',                         -- Name of the command
+    function(args) M.search(args) end,   -- Function defining what it does
+    {nargs = '?', complete = completion} -- Additional options
+    -- nargs can take '*' for any number of arguments, '?' for one or none,
+    -- and an integer value for exact numbers of arguments.
   )
 end
 
